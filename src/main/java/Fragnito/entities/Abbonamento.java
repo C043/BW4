e1e60de65bd959f6a695a3a377f9e4fb76517df0
C043 <mariofragnitoph@gmail.com>
2024-08-26 15:16:58 +0200

    Adds constructors

--- src/main/java/Fragnito/entities/Abbonamento.java
+++ src/main/java/Fragnito/entities/Abbonamento.java
@@ -13,4 +13,37 @@ public class Abbonamento extends Timbrabile {
     @Column(name = "periodo_abbonamento")
     @Enumerated(EnumType.STRING)
     private PeriodoAbbonamento periodoAbbonamento;
+
+    public Abbonamento() {
+    }
+
+    public Abbonamento(LocalDate dataEmissione, Distributore distributore, LocalDate dataScadenza, PeriodoAbbonamento periodoAbbonamento) {
+        super(dataEmissione, distributore);
+        this.dataScadenza = dataScadenza;
+        this.periodoAbbonamento = periodoAbbonamento;
+    }
+
+    public LocalDate getDataScadenza() {
+        return dataScadenza;
+    }
+
+    public void setDataScadenza(LocalDate dataScadenza) {
+        this.dataScadenza = dataScadenza;
+    }
+
+    public PeriodoAbbonamento getPeriodoAbbonamento() {
+        return periodoAbbonamento;
+    }
+
+    public void setPeriodoAbbonamento(PeriodoAbbonamento periodoAbbonamento) {
+        this.periodoAbbonamento = periodoAbbonamento;
+    }
+
+    @Override
+    public String toString() {
+        return "Abbonamento{" +
+                "dataScadenza=" + dataScadenza +
+                ", periodoAbbonamento=" + periodoAbbonamento +
+                "} " + super.toString();
+    }
 }
--- src/main/java/Fragnito/entities/Biglietto.java
+++ src/main/java/Fragnito/entities/Biglietto.java
@@ -11,6 +11,28 @@ import java.time.LocalDate;
 public class Biglietto extends Timbrabile {
     @Column(name = "data_vidimazione")
     private LocalDate dataVidimazione;
-    // Aggiungere relazione a viaggi
+    // Aggiungere relazione a viaggi e fare getter e setter e rifare toString() e constructor
 
+    public Biglietto() {
+    }
+
+    public Biglietto(LocalDate dataEmissione, Distributore distributore, LocalDate dataVidimazione) {
+        super(dataEmissione, distributore);
+        this.dataVidimazione = dataVidimazione;
+    }
+
+    public LocalDate getDataVidimazione() {
+        return dataVidimazione;
+    }
+
+    public void setDataVidimazione(LocalDate dataVidimazione) {
+        this.dataVidimazione = dataVidimazione;
+    }
+
+    @Override
+    public String toString() {
+        return "Biglietto{" +
+                "dataVidimazione=" + dataVidimazione +
+                "} " + super.toString();
+    }
 }
