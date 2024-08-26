eb88a002039ffae8b721e0558d213ccd86c43da0
C043 <mariofragnitoph@gmail.com>
2024-08-26 14:42:39 +0200

    Setted workspace

--- src/main/java/Fragnito/Application.java
+++ src/main/java/Fragnito/Application.java
@@ -1,8 +1,16 @@
 package Fragnito;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("BW4-Team-5");
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManager em = emf.createEntityManager();
+
+        em.close();
+        emf.close();
     }
 }
