ca0b8346265c66011bd9de2b75b8f68948601655
C043 <mariofragnitoph@gmail.com>
2024-08-27 11:15:41 +0200

    Update ViaggiDAO.java

--- src/main/java/Fragnito/dao/ViaggiDAO.java
+++ src/main/java/Fragnito/dao/ViaggiDAO.java
@@ -1,11 +1,11 @@
 package Fragnito.dao;
 
-import Fragnito.entities.Mezzo;
 import Fragnito.entities.Viaggio;
+import Fragnito.exceptions.NotFoundException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 
-import java.time.LocalDate;
+import java.util.UUID;
 
 public class ViaggiDAO {
     private final EntityManager em;
@@ -14,9 +14,26 @@ public class ViaggiDAO {
         this.em = em;
     }
 
-    public void save(Mezzo mezzo, LocalDate giorno, Integer tempoEffettivo) {
+    public void save(Viaggio viaggio) {
         EntityTransaction transaction = em.getTransaction();
         transaction.begin();
-        em.persist(new Viaggio());
+        em.persist(viaggio);
+        transaction.commit();
+        System.out.println("Viaggio " + viaggio.getId() + " salvato con successo!");
+    }
+
+    public Viaggio getViaggioById(UUID id) {
+        Viaggio found = em.find(Viaggio.class, id);
+        if (found == null) throw new NotFoundException(id);
+        return found;
+    }
+
+    public void delete(UUID id) {
+        Viaggio found = getViaggioById(id);
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        em.remove(found);
+        transaction.commit();
+        System.out.println("Viaggio " + found.getId() + " eliminato con successo!");
     }
 }
